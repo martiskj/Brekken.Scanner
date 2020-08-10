4758e2662b41b88a455af12a39b9c8cfb6ad5318
Martin Skjønhaug <martin.skjoenhaug@gmail.com>
2020-08-10 23:52:43 +0200

    Added static files

--- server/BrekkenScan.Api/Startup.cs
+++ server/BrekkenScan.Api/Startup.cs
@@ -42,6 +42,7 @@ namespace BrekkenScan.Api
                 app.UseHttpsRedirection();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseAuthorization();
